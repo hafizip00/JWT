0809970a4e4fb3a79eb70c5c0eb6f1b2614fa21c
hafizip00 <hafizip00@gmail.com>
2022-06-22 11:35:25 +0500

    Updating..

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,30 @@
 const router = require("express").Router();
 const User = require("../Models/User");
-
-//VALIDATION
-
-const Joi = require("@hapi/joi");
-
-const schema = Joi.object({
-  username: Joi.string().min(6).required(),
-  email: Joi.string().min(6).required().email(),
-  password: Joi.string().min(6).required(),
-});
+const registerSchema = require('../validation')
+const loginValidation = require('../validation')
+const bcrypt = require('bcrypt')
 
 router.post("/register", async (req, res) => {
-  // validate before adding to DB
 
-  const {error} = schema.validate(req.body);
+  // validate before adding to DB
+  const {error} = registerSchema(req.body);
+
+  const emailExist = await User.findOne({email : req.body.email})
+  if (emailExist) {
+    return res.status(400).send("Already Exist")
+  }
 
   if (error) {
       return res.status(400).send(error.details[0].message)
   }
+
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(req.body.password , salt)
+  console.log(hashedPassword)
   const user = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
   try {
     const savedUser = await user.save();
@@ -31,5 +33,27 @@ router.post("/register", async (req, res) => {
     res.json("ERROR");
   }
 });
+
+
+router.post('/login' ,async (req, res)=>{
+  const {error} = loginValidation(req.body)
+  if(error){
+    return res.status(403).send(error.details[0].message)
+  }
+  const isemailExist = await User.findOne({email : req.body.email});
+  if (!isemailExist) {
+    res.status(500).json("Email Or password is Wrong")
+  }
+  try {
+  const isPasswordCorrect = await bcrypt.compare(req.body.password ,isemailExist.password)
+  if(isPasswordCorrect){
+    res.status(200).json(isemailExist)
+  }else{
+    res.status(500).json("Credientials mismatch")
+  }
+  } catch (error) {
+    res.status(400).json("Some error Occurred")
+  }
+})
 
 module.exports = router;
